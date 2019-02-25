9f985ac29b6b038e0e2f47bf01398ebd2dd91a97
root <root@pub-controller-001.hanpda.com>
2019-02-25 09:20:01 +0900

    minor update

--- view-openstack-network-connectivity.py
+++ view-openstack-network-connectivity.py
@@ -37,70 +37,13 @@ def exec_ssh(ssh_hostname, ssh_cmd):
 def isStrBlank (myString):
     return not (myString and myString.strip())
 
-def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
-
-    '''
-    From Joel's answer at https://stackoverflow.com/a/29597209/2966723 
-
-    If the graph is a tree this will return the positions to plot this in a 
-    hierarchical layout.
-
-    G: the graph (must be a tree)
-
-    root: the root node of current branch 
-    - if the tree is directed and this is not given, the root will be found and used
-    - if the tree is directed and this is given, then the positions will be just for the descendants of this node.
-    - if the tree is undirected and not given, then a random choice will be used.
-
-    width: horizontal space allocated for this branch - avoids overlap with other branches
-
-    vert_gap: gap between levels of hierarchy
-
-    vert_loc: vertical location of root
-
-    xcenter: horizontal location of root
-    '''
-    if not nx.is_tree(G):
-        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
-
-    if root is None:
-        if isinstance(G, nx.DiGraph):
-            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
-        else:
-            root = random.choice(list(G.nodes))
-
-    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
-        '''
-        see hierarchy_pos docstring for most arguments
-
-        pos: a dict saying where all nodes go if they have been assigned
-        parent: parent of this branch. - only affects it if non-directed
-
-        '''
-
-        if pos is None:
-            pos = {root:(xcenter,vert_loc)}
-        else:
-            pos[root] = (xcenter, vert_loc)
-        children = list(G.neighbors(root))
-        if not isinstance(G, nx.DiGraph) and parent is not None:
-            children.remove(parent)  
-        if len(children)!=0:
-            dx = width/len(children) 
-            nextx = xcenter - width/2 - dx/2
-            for child in children:
-                nextx += dx
-                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap, 
-                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
-                                    pos=pos, parent = root)
-        return pos
-
-
-    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
-
+#####################################################################################
 
 if __name__ == '__main__':
 
+	###############################################
+	### Raw 데이터 생성
+	###############################################
 	result = []
 
 	hostnames = (
@@ -220,7 +163,9 @@ if __name__ == '__main__':
 		#print(result)
 
 
-	## 이미지 생성 작업 시작
+	###############################################
+	### 이미지 생성 작업 시작
+	###############################################
 	G = nx.Graph() 
 
 	for interface in result:
@@ -279,12 +224,12 @@ if __name__ == '__main__':
 	#print(G.edges())
 
 	#pos = nx.shell_layout(G)  # positions for all nodes
-	#pos = nx.spring_layout(G, k=0.05, iterations=40)  # positions for all nodes
+	pos = nx.spring_layout(G, k=0.05, iterations=40)  # positions for all nodes
 	#pos = nx.spring_layout(G, iterations=50)
 	#pos = nx.spectral_layout(G, scale=2)  # positions for all nodes
 	#pos = nx.circular_layout(G)  # positions for all nodes
 	#pos = nx.random_layout(G)  # positions for all nodes
-	pos = hierarchy_pos(G, "B:br-ex(pub-compute-001)")
+	#pos = hierarchy_pos(G, "B:br-ex(pub-compute-001)")
 
 	## Node 종류(Interface/Port/Bridge)별 목록 생성
 	nodes_interface = [node for node in G.nodes() if node.startswith("I:")]
